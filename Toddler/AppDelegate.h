ed465e6252c965ac52b5e6e4bd28f6528080a37d
Dustrise <dustrise@Dustrises-Mac-mini-3.local>
2013-03-25 17:05:33 +0530

    Initial Commit

---
+++ Toddler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Toddler
+//
+//  Created by Dustrise on 3/25/13.
+//  Copyright (c) 2013 Dustrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Toddler/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Toddler
+//
+//  Created by Dustrise on 3/25/13.
+//  Copyright (c) 2013 Dustrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
